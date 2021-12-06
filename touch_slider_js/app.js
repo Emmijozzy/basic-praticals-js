87c570b0386f79db334a5b689964fcee0379be70
Emmijozzy <josephogunsuyi360@gmail.com>
2021-12-06 22:27:59 +0100

    updatetouch slide

--- touch_slider_js/app.js
+++ touch_slider_js/app.js
@@ -1,78 +1,95 @@
 
 const slider = document.querySelector('.slide-container'),
-    slides = Array.from(document.querySelectorAll('.slide'));
+  slides = Array.from(document.querySelectorAll('.slide'))
 
 let isDragging = false,
-    startPos = 0,
-    currentIndex = 0,
-    currentTranslate = 0,
-    prevTranslate = 0,
-    animationID = 0;
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID = 0,
+  currentIndex = 0
 
-    slides.forEach((slide, index) => {
-        // console.log(slide, index);
-        slide.addEventListener('dragstart', (e) => {
-            e.preventDefault()
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img')
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault())
 
-        //touch events
-        slide.addEventListener('touchStart', touchStart(index))
-        slide.addEventListener('touchend', touchEnd)
-        slide.addEventListener('touchmove', touchMove)
+  // Touch events
+  slide.addEventListener('touchstart', touchStart(index))
+  slide.addEventListener('touchend', touchEnd)
+  slide.addEventListener('touchmove', touchMove)
 
-        //mouse events 
-        slide.addEventListener('mousedown', touchStart(index))
-        slide.addEventListener('mouseup', touchEnd)
-        slide.addEventListener('mouseleave', touchEnd)
-        slide.addEventListener('mousemove', touchMove)
-        })
-    })
+  // Mouse events
+  slide.addEventListener('mousedown', touchStart(index))
+  slide.addEventListener('mouseup', touchEnd)
+  slide.addEventListener('mouseleave', touchEnd)
+  slide.addEventListener('mousemove', touchMove)
+})
 
-    function touchStart(index) {
-        return e => {
-            isDragging = true;
-            currentIndex = index;
-            startPos = getPositionX(e)
-            console.log(startPos , 'start')
-            animation()
-            slider.classList.add('grapping')
-        }
-    }
+// Disable context menu
+window.oncontextmenu = function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+  return false
+}
+
+function touchStart(index) {
+  return function (event) {
+    // console.log(index)
+    isDragging = true
+    currentIndex = index
+    startPos = getPositionX(event)
+    // console.log(startPos)
+    animation()
+
+    // https://css-tricks.com/using-requestanimationframe/
+    slider.classList.add('grabbing')
+  }
+}
+
+function touchEnd() {
+  isDragging = false;
+  cancelAnimationFrame(animationID)
+  // console.log('cancled')
+  
+  const movedBy = currentTranslate - prevTranslate
+  // console.log("move", movedBy)
+  
+  if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1
+  
+  if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
+  
+  setPositionByIndex()
+  
+  slider.classList.remove('grabbing')
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event)
+    // console.log(currentPosition)
+    currentTranslate = prevTranslate + currentPosition - startPos
+  }
+}
+
+function getPositionX(event) {
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+}
+
+function animation() {
+  setSliderPosition()
+  // console.log('woking anima')
     
-    function touchEnd() {
-        isDragging  = false
-        // cancelAnimationFrame(animationID)
-        slider.classList.remove('grapping')
-        console.log('end')
-        const moveBy = currentTranslate - prevTranslate;
-        if (moveBy < -100 && currentIndex < slides.length - 1)  currentIndex++
-        if (moveBy > 100 && currentIndex > 0)  currentIndex--
-        setPositionByIndex()
-    }
+  if (isDragging) /*requestAnimationFrame(animation)*/animationID = requestAnimationFrame(animation)
+  // console.log('working')
+}
 
-    function touchMove(e) {
-        console.log('move')
-        if (isDragging) {
-            const currentPosition = getPositionX(e)
-            console.log(currentPosition, "current")
-            currentTranslate = prevTranslate + currentPosition  - startPos
-        }
-    }
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`
+}
 
-    function getPositionX(event){
-        return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
-    }
-    function animation() {
-        setSliderPosition() 
-        if(isDragging) 
-        requestAnimationFrame(animation)
-
-        console.log('working ani')
-    };
-    function setSliderPosition() {
-        slider.style.transform = `translateX(${currentTranslate}px)`
-    }
-    function setPositionByIndex() {
-        currentTranslate = currentIndex* -window.innerWidth
-        prevTranslate = currentTranslate;
-        setSliderPosition()
-    }
+function setPositionByIndex() {
+  console.log(-window.innerWidth)
+  currentTranslate = currentIndex * -window.innerWidth
+  prevTranslate = currentTranslate
+  setSliderPosition()
+}
